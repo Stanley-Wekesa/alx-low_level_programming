84c0c830e411f6ead9a4dea82c3ed58b34531007
Stanley-Wekesa <stanleywekesa22@gmail.com>
2023-03-16 17:44:25 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * my_function - This is a description
+ * main - This is my main function
+ * Return: Zero
  */
-int main(void) {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
