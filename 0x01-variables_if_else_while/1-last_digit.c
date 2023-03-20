aac26d80f799a71d2f2873439547a09b89246b5a
Stanley-Wekesa <stanleywekesa22@gmail.com>
2023-03-20 16:46:08 -0700

    Commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,10 +4,6 @@
 
 /**
  * main - Entry point
- *
- * This program will assign a random number to the variable n each time it is executed.
- * The program will print the last digit of the number stored in the variable n.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
