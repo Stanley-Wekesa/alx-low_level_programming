6ad7e1de54160ae991e95c30e27b1166e6d38c41
Stanley-Wekesa <stanleywekesa22@gmail.com>
2023-03-20 17:52:58 -0700

    Commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of digit
- * Return: Always 0 (Success)
+ * main - Prints all possible combinations of single-digit numbers.
+ * Return: Always 0.
  */
 int main(void)
 {
-int i, j;
+int digit;
 
-for (i = 0; i < 10; i++)
+for (digit = 0; digit < 10; digit++)
 {
-for (j = i; j < 10; j++)
-{
-if (i != j)
-{
-putchar(i + '0');
-putchar(j + '0');
+putchar(digit + '0');
 
-if (i != 8 || j != 9)
+/* If not the last digit, print comma and space */
+if (digit != 9)
 {
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 
