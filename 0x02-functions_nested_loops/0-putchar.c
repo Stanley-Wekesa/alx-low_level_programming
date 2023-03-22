0a4f26e384f4d583162bdb65f589f8b26ba4d236
Stanley-Wekesa <stanleywekesa22@gmail.com>
2023-03-21 18:31:55 -0700

    Printing putchar_ Program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* Print "_putchar" followed by a newline */
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+}
+""
